942233f2ec835bfb2960090eff42b9622cc90d9d
unknown <vanzandt.school.email@gmail.com>
2024-01-30 13:14:03 -0500

    Updated models, to add blog model.

--- Develop/models/User.js
+++ Develop/models/User.js
@@ -16,17 +16,9 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
     },
     password: {
       type: DataTypes.STRING,
@@ -36,11 +28,16 @@ User.init(
       },
     },
   },
+
   {
     hooks: {
       beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
       },
     },
     sequelize,
